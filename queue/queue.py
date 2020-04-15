16f0b5c6402cd25bea9b99217894457305ae6d04
ms.kim <iris4865@gmail.com>
2020-04-15 19:00:59 +0900

    Update queue.py

---
+++ queue/queue.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data: object):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+    def __str__(self):
+        return str(self.data)
+
+    def __repr__(self):
+        return str(self.data)
+
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def put(self, data: object):
+        new_node = Node(data)
+
+        if self.tail:
+            self.tail.next = new_node
+        else:
+            self.head = new_node
+
+        self.tail = new_node
+        self.size += 1
+
+    def get(self) -> object:
+        data = self.head.data
+        self.head = self.head.next
+
+        self.size -= 1
+        return data
+
+    def clear(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def __iter__(self):
+        current = self.head
+        while current is not None:
+            yield current.data
+            current = current.next
+    
+    def __reversed__(self):
+        return reversed([data for data in self])
+
+    def __len__(self) -> int:
+        return self.size
